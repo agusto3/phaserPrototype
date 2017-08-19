99d03a1753a2073e28c621f7b7a8d800d1134a0b
Agustin Leira (windows Sony vaio) <agustinleira1@hotmail.com>
2017-08-19 18:01:14 -0300

    trying out some stuff

--- FirstGame/js/stateMain.js
+++ FirstGame/js/stateMain.js
@@ -11,53 +11,47 @@ var StateMain = {
         game.stage.backgroundColor = "#00ffff";
         //add the ground
         this.ground = game.add.sprite(0, game.height * .9, "ground");
-		
         //add the hero in 
         this.hero = game.add.sprite(game.width * .2, this.ground.y - 25, "hero");
         //add the power bar just above the head of the hero
         this.powerBar = game.add.sprite(this.hero.x + 25, this.hero.y - 25, "bar");
         this.powerBar.width = 0;
-       
-        
-		
-        
-
-		//start the physics engine
+        //start the physics engine
         game.physics.startSystem(Phaser.Physics.ARCADE);
         //enable the hero for physics
         game.physics.enable(this.hero, Phaser.Physics.ARCADE);
         game.physics.enable(this.ground, Phaser.Physics.ARCADE);
-		
-		this.hero.body.gravity.y = 200;
+        //game.physics.arcade.gravity.y = 100;
+        this.hero.body.gravity.y = 200;
         this.hero.body.collideWorldBounds = true;
+        //this.hero.body.bounce.set(0, .2);
         this.ground.body.immovable = true;
-		
-		//record the initial position
-		this.startY = this.hero.y;
-		
-		//set listeners
-		game.input.onDown.add(this.mouseDown, this);
-		
-		this.blocks = game.add.group();
-		this.makeBlocks();
+        //record the initial position
+        this.startY = this.hero.y;
+        //set listeners
+        game.input.onDown.add(this.mouseDown, this);
+
+        this.blocks = game.add.group();
+        this.makeBlocks();
+        this.blocks.x = game.width - this.blocks.width;
+        this.blocks.y=this.ground.y-50;
+        
     },
     mouseDown: function() {
-		
-		if (this.hero.y != this.startY) {
-			return;
-		}
-		
-		game.input.onDown.remove(this.mouseDown, this);
+        if (this.hero.y != this.startY) {
+            return;
+        }
+        game.input.onDown.remove(this.mouseDown, this);
         this.timer = game.time.events.loop(Phaser.Timer.SECOND / 1000, this.increasePower, this);
-		game.input.onUp.add(this.mouseUp, this);
+        game.input.onUp.add(this.mouseUp, this);
     },
     mouseUp: function() {
-		game.input.onUp.add(this.mouseUp, this);
-		this.doJump();
+        game.input.onUp.remove(this.mouseUp, this);
+        this.doJump();
         game.time.events.remove(this.timer);
         this.power = 0;
         this.powerBar.width = 0;
-		game.input.onDown.add(this.mouseDown, this);
+        game.input.onDown.add(this.mouseDown, this);
     },
     increasePower: function() {
         this.power++;
@@ -66,21 +60,17 @@ var StateMain = {
             this.power = 50;
         }
     },
-	doJump: function() {
+    doJump: function() {
         this.hero.body.velocity.y = -this.power * 12;
     },
-    update: function() {
-		 game.physics.arcade.collide(this.hero, this.ground);
-	
-	},
-	makeBlocks: function() {
+    makeBlocks: function() {
         var wallHeight=game.rnd.integerInRange(2, 6);
         for (var i = 0; i < wallHeight; i++) {
             var block = game.add.sprite(0, -i * 25, "block");
             this.blocks.add(block);
         }
     },
-	moveBlocks: function() {
-        console.log("moving through");
+    update: function() {
+        game.physics.arcade.collide(this.hero, this.ground);
     }
 }

b616c85260e358811baece36251ef6edb34bf493
Agustin Leira (windows Sony vaio) <agustinleira1@hotmail.com>
2017-08-20 15:28:24 -0300

    perry is in the hood 22

--- FirstGame/js/stateMain.js
+++ FirstGame/js/stateMain.js
@@ -8,7 +8,7 @@ var StateMain = {
         game.load.image("playAgain", "images/playAgain.png");
         game.load.image("clouds", "images/clouds.png");
 		//game.load.spritesheet('mummy', 'images/Metalslug_mummy37x45.png', 37, 45, 18);
-		game.load.spritesheet('mummy', 'images/dog.png', 45, 25, 25);
+		game.load.spritesheet('mummy', 'images/dog.png', 48, 27, 25);
     },
     create: function() {
         this.clickLock = false;
